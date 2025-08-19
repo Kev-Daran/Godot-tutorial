3372658aa6d115a51840bfc8d6d01f95a3c67930
Dev Karan <devkaran7501@gmail.com>
2025-08-19 21:01:08 +0530

    learning

--- game-tut/scenes/levels/logo.gd
+++ game-tut/scenes/levels/logo.gd
@@ -1,13 +1,17 @@
 extends Sprite2D
 
 var pos : Vector2 = Vector2.ZERO
+var x_movement = 10
 var test_scale : Vector2 = Vector2(1,1)
 var scale_increment : float = 0.01
 
 func _ready() -> void:
 	pos += Vector2(500, 300)
 	position = pos
+	scale = test_scale
 	
 func _process(delta: float) -> void:
-	test_scale += Vector2(scale_increment, scale_increment)
-	scale = test_scale
+	pos.x += x_movement
+	position = pos
+	#test_scale += Vector2(scale_increment, scale_increment)
+	#scale = test_scale
